79c30d5f9a686af1888e512061d97a443ec68b83
stvogt <savogt@protonmail.com>
2022-01-26 00:29:05 -0300

    apply black

--- beep/beep.py
+++ beep/beep.py
@@ -1,29 +1,398 @@
-"""Provide the primary functions."""
+import qcportal as ptl
+import pandas as pd
+import numpy as np
+from .be_tools import zpve_correction
+from exceptions import *
+
+# Author: svogt, gbovolenta
+# Date: 15.06.2021
 
 
-def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format).
-
-    Replace this function and doc string for your own project.
+class BindingParadise(object):
+    """Data query QCPortal wrapper for the BEEP  database.
 
     Parameters
     ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from.
+    None
 
-    Returns
-    -------
-    quote : str
-        Compiled string including quote and optional attribution.
+    Attributes
+    ----------
+    client : qcportal.client.FractalClient
+        The FractalClient instance
+
+    mol: str
+        The supermolecule name.
+
+    opt_lot: list
+        The list of optimization methods for which the supermolecule has been optimized.
+
+
+    df_be: Pandas.DataFrame
+         The dataframe containing the binding energy values for the supermolecule.
+
     """
 
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
+    def __init__(self, username, password):
+        client = ptl.FractalClient(
+            address="152.74.10.245:7777",
+            verify=False,
+            username=username,
+            password=password,
+        )
+        self.client = client
+
+    def molecules_list(self):
+        """Lists all the available molecules:
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        list
+            A list object which contains the available molecules."""
+
+        client = self.client
+        c = client.list_collections().loc["ReactionDataset"]
+        molecules_list = []
+        for row in c.index:
+            if "be_" and "W22" in row:
+                r = str(row.split("_")[1]) + "_" + str(row.split("_")[2])
+                if r not in molecules_list:
+                    molecules_list.append(r)
+        return molecules_list
+
+    def set_molecule(self, mol):
+        """Sets instance attribute molecule:
+
+        Parameters
+        ----------
+
+        mol : str
+            The selected molecule
+
+        Returns
+        -------
+        None
+        """
+
+        print("Molecule set: {}".format(mol.upper()))
+        self._mol = mol
+
+    def get_optimization_methods(self):
+        """Lists the optimization methods for which the molecule has been optimized:
+
+        Parameters
+        ----------
+
+        None
+
+        Returns
+        -------
+        list
+            A list object with the available optimization methods."""
+
+        client = self.client
+
+        try:
+            self._mol
+        except AttributeError:
+            raise MoleculeNotSetError()
+
+        mol = self._mol
+        c = client.list_collections("ReactionDataset")
+        be_col = [row for row in c.index if "be_" + mol + "_05_" in row[1]]
+        opt_lot = [x[1].split("_")[-1].upper() for x in be_col]
+        self._opt_lot_list = opt_lot
+        print("List of optimization methods: {}".format(opt_lot))
+        return opt_lot
+
+    def load_data(self, opt_lot=None):
+        """Loads binding energy data for the selected molecule:
+
+        Parameters
+        ----------
+
+        opt_lot : str, optional
+            The selected optimization method. When ``opt_lot = None``, sets the first available level of theory.
+        Returns
+        -------
+        None
+
+        Raises
+        -------
+        MoleculeNotSetError
+            If molecule is not set to the instance.
+        OptimizationMethodNotSetError
+            If Optimization Method is not set yet.
+        """
+
+        client = self.client
+
+        try:
+            self._opt_lot_list
+        except AttributeError:
+            raise OptimizationMethodNotSetError()
+
+        if opt_lot == None:
+            opt_lot = self._opt_lot_list[0]
+
+        ds_be_list = []
+        for i in range(1, 20):
+            be_name = "be_" + self._mol + "_" + "%02d" % i + "_" + opt_lot
+            try:
+                ds_be = client.get_collection("ReactionDataset", be_name)
+            except KeyError:
+                continue
+            ds_be_list.append(ds_be)
+        self._ds_set = ds_be_list
+        self._opt_lot = opt_lot
+
+    def get_methods(self):
+        """Gets the available binding energy methods:
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        dict
+            A dictionary object with method and corresponding stoichiometry.
+        """
+
+        method_dict = {}
+        client = self.client
+        mol = self._mol
+        ds_be = self._ds_set[-1]
+        method_info = ds_be.data.history
+        for m in method_info:
+            method = m[2]
+            stoich = m[-1]
+            method_dict[method] = stoich
+        print(
+            "The available binding energy methods for {} geometries are : {}".format(
+                self._opt_lot, [x for x in method_dict.keys()]
+            )
+        )
+        return method_dict
+
+    def get_zpve_correction(self):
+        """Returns a list with Zero Point Vibrational Energy correction factors. These have been obtained at HF-3c/MINIX level of theory, using a linear model. In order to correct the binding energy (BE), use:
+
+        ``bp = BindingParadise('username ','password')
+          BE = bp.get_zpve_correction()[0] * BE + bp.get_zpve_correction()[1]``
+
+        Parameters
+        ----------
+
+        None
+
+        Returns
+        -------
+        list
+            A list with with Zero Point Vibrational Energy correction factors.
+        """
+        try:
+            self._mol
+        except AttributeError:
+            raise MoleculeNotSetError()
+
+        return zpve_correction[self._mol]
+
+    def get_values(self, method=None, zpve=False):
+        """Gets binding energy values for a given method.
+
+        Parameters
+        ----------
+
+        method : str or list, optional
+            The methods used to compute the binding energies.  When ``method = None``, sets the first available level of theory. Use get_be_methods to list the available binding energy methods.
+        zpve : bool, optional
+            Returns the binding energy values corrected for Zero Point Vibrational Energy.
+
+        Returns
+        -------
+        Pandas.DataFrame
+            A Pandas.DataFrame object containing the binding energy values.
+        """
+
+        try:
+            self._mol
+        except AttributeError:
+            raise MoleculeNotSetError()
+
+        try:
+            self._ds_set
+        except AttributeError:
+            raise DataNotLoadedError()
+
+        en_val_list = []
+        method_dict = self.get_methods()
+
+        if not method:
+            method = list(method_dict.keys())[0]
+        stoich = method_dict[method]
+
+        for ds in self._ds_set:
+            en_val = ds.get_values(stoich=stoich, method=method)
+            en_val_list.append(en_val)
+        df_be = pd.concat(en_val_list).dropna()
+
+        if zpve:
+            m, b = zpve_correction[self._mol]
+            df_be[str(method.upper()) + " + ZPVE"] = m * df_be + b
+
+        self._df_be = df_be
+        return df_be
+
+    def get_molecules(self, method=None):
+        """Help on method set_molecule in class BEEP:
+
+        Parameters
+        ----------
+
+        collection_type : str
+            The collection type to be accessed
+        name : str
+            The name of the collection to be accessed
+        full_return : bool, optional
+            Returns the full server response if True that contains additional metadata.
+        include : QueryListStr, optional
+            Return only these columns.
+        exclude : QueryListStr, optional
+            Return all but these columns.
+        Returns
+        -------
+        Collection
+            A Collection object if the given collection was found otherwise returns `None`.
+        """
+
+        mol_dict = {}
+        mol_list = []
+        name_list = []
+        method_dict = self.get_methods()
+
+        if not method:
+            method = list(method_dict.keys())[0]
+
+        stoich = method_dict[method]
+
+        method_base = method
+
+        # if isinstance(self._df_be, type(None)):
+        self._df_be = self.get_values()
+
+        if "D3BJ" in method.upper():
+            method_base = method.split("-")[0]
+
+        for ds in self._ds_set:
+            rr = ds.get_records(method=method_base)
+            idx = rr.index
+            for i in range(len(idx)):
+                mol_frag = rr.index[i][2]
+                if mol_frag == 0:
+                    mol_name = idx[i][0]
+                    mol_id = rr.loc[idx[i]]["Molecule"]
+                    mol_list.append(mol_id)
+                    name_list.append(mol_name)
+        mol_obj_list = self.client.query_molecules(mol_list)
+        for i in range(len(mol_obj_list)):
+            mol_dict[name_list[i]] = {"Molecule": mol_obj_list[i]}
+        df_mol = pd.DataFrame.from_dict(mol_dict, orient="index")
+        return pd.concat([self._df_be, df_mol], axis=1)
+
+    def visualize(self, methods=None):  # bin como opcion (numero)
+        """Help on method set_molecule in class BEEP:
+
+        Parameters
+        ----------
+
+        collection_type : str
+            The collection type to be accessed
+        name : str
+            The name of the collection to be accessed
+        full_return : bool, optional
+            Returns the full server response if True that contains additional metadata.
+        include : QueryListStr, optional
+            Return only these columns.
+        exclude : QueryListStr, optional
+            Return all but these columns.
+        Returns
+        -------
+        Collection
+            A Collection object if the given collection was found otherwise returns `None`.
+        """
+
+        import matplotlib.pyplot as plt
+        import numpy as np
+        import math
+        import qcelemental as qcel
+
+        if not method:
+            method = list(method_dict.keys())[0]
+
+        stoich = method_dict[method]
+
+        method_base = method
+
+        df_be = self._df_be
+        mol = self._mol
+        nbins = 12
+
+        bins_K = np.arange(
+            min(df_be.min(axis=1)), 1, 0.3
+        ) * qcel.constants.conversion_factor("kcal/mol", "K")
+
+        fig = plt.figure(figsize=(12, 10))
+        fig.subplots_adjust(hspace=0.8)
+        for i in range(len(methods)):
+            v_K = df_be[
+                methods[i] + "/def2-tzvp"
+            ].values * qcel.constants.conversion_factor("kcal/mol", "K")
+
+            # Counting nan elements in order to exclude them from structures' count:
+            n = list(map(lambda x: 0.0 if math.isnan(x) else x, v_K))
+            nan_structs = n.count(0.0)
+
+            fig.add_subplot(len(methods), 1, i + 1)
+
+            plt.hist(
+                v_K,
+                bins=nbins,
+                edgecolor="black",
+                density=False,
+                color="blue",
+                alpha=0.3,
+            )
+            plt.xlabel("$E_b$ / $K$ ", size=15)
+            plt.ylabel("frequency number", size=15)
+            plt.title(
+                "{}, {}/def2-tzvp Binding Energy values, {} optimized structures".format(
+                    mol.upper(), methods[i], str(len(v_K) - nan_structs)
+                ),
+                size=16,
+            )
+        plt.show()
+
+    def get_sets(self, methods=None):
+        """Gets a list of all ReactionDatasets for the selected molecule.
+
+        Parameters
+        ----------
+
+        method : str or list, optional
+            The methods used to compute the binding energies.  When ``method = None``, sets the first available level of theory. Use get_be_methods to list the available binding energy methods.
+
+        Returns
+        -------
+        list
+            A list object containing ReactionDatasets for the selected molecule.
+        """
+
+        return self._ds_set
 
 
 if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())
+    print(BindingParadies())
--- beep/binding_energy_compute.py
+++ beep/binding_energy_compute.py
@@ -1,13 +1,15 @@
 import sys, time
-#sys.path.append('/home/svogt/fractal/site_finder')
-#from molecule_sampler import molecule_sampler as  mol_sample
+
+# sys.path.append('/home/svogt/fractal/site_finder')
+# from molecule_sampler import molecule_sampler as  mol_sample
 import qcfractal.interface as ptl
 from pathlib import Path
 
 
 def print_out(string, out_file):
-    with open(out_file, 'a') as f:
+    with open(out_file, "a") as f:
         f.write(string)
+
 
 def rmsd_filter(ds_opt, opt_lot, o_file):
 
@@ -17,7 +19,9 @@ def rmsd_filter(ds_opt, opt_lot, o_file):
 
     for i in ds_opt.df.index:
         try:
-            di[i]= ds_opt.get_record(name = i, specification = opt_lot).get_final_molecule()
+            di[i] = ds_opt.get_record(
+                name=i, specification=opt_lot
+            ).get_final_molecule()
         except:
             "ValidationError" or "TypeError"
             continue
@@ -26,19 +30,18 @@ def rmsd_filter(ds_opt, opt_lot, o_file):
 
     count = 0
     for i in range(len(list_keys)):
-        for j in range(i+1,len(list_keys)):
-            count = count+1
-            mol1=di[list_keys[i]]
-            mol2=di[list_keys[j]]
-            rmsd = mol1.align(mol2, atoms_map=True)[1]['rmsd']
-            #print(rmsd)
+        for j in range(i + 1, len(list_keys)):
+            count = count + 1
+            mol1 = di[list_keys[i]]
+            mol2 = di[list_keys[j]]
+            rmsd = mol1.align(mol2, atoms_map=True)[1]["rmsd"]
+            # print(rmsd)
             if rmsd < 0.25 and rmsd != 0.0:
                 if not list_keys[j] in todelete:
                     todelete.append(list_keys[j])
-    print_out("List of molecules to delete: {}\n".format(todelete),o_file)
+    print_out("List of molecules to delete: {}\n".format(todelete), o_file)
 
     from collections import Counter
-
 
     c = Counter(todelete)
     fin = todelete.copy()
@@ -52,7 +55,9 @@ def rmsd_filter(ds_opt, opt_lot, o_file):
     return di, final_keys
 
 
-def be_stoich(ds_be, database, small_collection, wat_collection, opt_lot, o_file, client):
+def be_stoich(
+    ds_be, database, small_collection, wat_collection, opt_lot, o_file, client
+):
 
     ds_opt = client.get_collection("OptimizationDataset", database)
     ds_m = client.get_collection("OptimizationDataset", small_collection)
@@ -65,78 +70,105 @@ def be_stoich(ds_be, database, small_collection, wat_collection, opt_lot, o_file
         g = mol.geometry.flatten()
         s = mol.symbols
 
-        m = database.split('_')[0]
-        op_m = ds_m.get_record(name = m, specification = opt_lot)
+        m = database.split("_")[0]
+        op_m = ds_m.get_record(name=m, specification=opt_lot)
         m_2 = op_m.get_final_molecule()
 
-        w = database.split('_')[1]+"_"+database.split('_')[2]
+        w = database.split("_")[1] + "_" + database.split("_")[2]
 
-        m_w = ds_w.get_record(name = w, specification = opt_lot)
+        m_w = ds_w.get_record(name=w, specification=opt_lot)
         m_1 = m_w.get_final_molecule()
         g1 = m_1.geometry.flatten()
         s1 = m_1.symbols
 
         n1 = len(s1)
 
-
         d = ptl.Molecule(
-        symbols=s,
-        geometry = g,
-        fragments = [list(range(0,n1)),list(range(n1,len(s)))]
+            symbols=s,
+            geometry=g,
+            fragments=[list(range(0, n1)), list(range(n1, len(s)))],
         )
 
-        j5 = d.get_fragment(0)  #M1
+        j5 = d.get_fragment(0)  # M1
         j4 = d.get_fragment(1)
-        j7 = d.get_fragment(0,1)  # M2
-        j6 = d.get_fragment(1,0)
+        j7 = d.get_fragment(0, 1)  # M2
+        j6 = d.get_fragment(1, 0)
 
         be_cal = {
-            "default":[(d,1.0),(j4,1.0),(j5,1.0),(j7,-1.0),(j6,-1.0),(m_1,-1.0),(m_2,-1.0)],
-
-            "be_nocp": [(d,1.0),(m_1,-1.0),(m_2,-1.0)]
-            #"ie": [(d,1.0),(j7,-1.0),(j6,-1.0)],
-
-            #"de":[(m_1,1.0),(m_2,1.0),(j4,-1.0),(j5,-1.0)], 
-
-            }
+            "default": [
+                (d, 1.0),
+                (j4, 1.0),
+                (j5, 1.0),
+                (j7, -1.0),
+                (j6, -1.0),
+                (m_1, -1.0),
+                (m_2, -1.0),
+            ],
+            "be_nocp": [(d, 1.0), (m_1, -1.0), (m_2, -1.0)]
+            # "ie": [(d,1.0),(j7,-1.0),(j6,-1.0)],
+            # "de":[(m_1,1.0),(m_2,1.0),(j4,-1.0),(j5,-1.0)],
+        }
         ds_be.add_rxn(k, be_cal)
         ds_be.save()
 
-def compute_be(wat_collection, small_collection, database, opt_lot, lot, o_file, client):
-    name_be = "be_"+str(database)+"_"+opt_lot.split("_")[0]
+
+def compute_be(
+    wat_collection, small_collection, database, opt_lot, lot, o_file, client
+):
+    name_be = "be_" + str(database) + "_" + opt_lot.split("_")[0]
 
     try:
-        ds_be = ptl.collections.ReactionDataset(name_be, ds_type="rxn", client=client, default_program="psi4")
+        ds_be = ptl.collections.ReactionDataset(
+            name_be, ds_type="rxn", client=client, default_program="psi4"
+        )
         ds_be.save()
 
-        be_stoich(ds_be, database, small_collection, wat_collection, opt_lot, o_file, client=client)
+        be_stoich(
+            ds_be,
+            database,
+            small_collection,
+            wat_collection,
+            opt_lot,
+            o_file,
+            client=client,
+        )
 
     except KeyError:
         print_out("Be database {} already exists\n".format(str(name_be)), o_file)
 
-
         ds_be = client.get_collection("ReactionDataset", name_be)
 
-    if not ds_be.get_values(method = lot.split("_")[0], basis = lot.split("_")[1]).empty:
-        print_out("{} Be values already computed in {}\n".format(str(lot), str(database)),o_file)
+    if not ds_be.get_values(method=lot.split("_")[0], basis=lot.split("_")[1]).empty:
+        print_out(
+            "{} Be values already computed in {}\n".format(str(lot), str(database)),
+            o_file,
+        )
         return None
 
-    #ds_be.set_default_program("psi4")
-    #keywords= ptl.models.KeywordSet(values= {'reference' : 'uks'})
+    # ds_be.set_default_program("psi4")
+    # keywords= ptl.models.KeywordSet(values= {'reference' : 'uks'})
 
-    #ds_be.add_keywords("rad_be", "psi4", keywords, default= True)
+    # ds_be.add_keywords("rad_be", "psi4", keywords, default= True)
 
-    #ds_be.save()
+    # ds_be.save()
 
-    #c = ds_be.compute(lot.split("_")[0], lot.split("_")[1], keywords="rad_be", stoich="default", tag='be_comp')
-    c = ds_be.compute(lot.split("_")[0], lot.split("_")[1], stoich="default", tag='be_comp')
-    c1 = ds_be.compute('hf3c', 'minix', stoich='be_nocp',tag='be_hf3c')
+    # c = ds_be.compute(lot.split("_")[0], lot.split("_")[1], keywords="rad_be", stoich="default", tag='be_comp')
+    c = ds_be.compute(
+        lot.split("_")[0], lot.split("_")[1], stoich="default", tag="be_comp"
+    )
+    c1 = ds_be.compute("hf3c", "minix", stoich="be_nocp", tag="be_hf3c")
     print_out("Collection {}: {}, {}\n".format(name_be, c, c1), o_file)
 
-    f_w = open('/home/astrochem/energy_compute/job_ids/' +ds_be.name+'_'+lot.split("_")[0]+'_idlist.dat', 'w')
-    id_str = ''
+    f_w = open(
+        "/home/astrochem/energy_compute/job_ids/"
+        + ds_be.name
+        + "_"
+        + lot.split("_")[0]
+        + "_idlist.dat",
+        "w",
+    )
+    id_str = ""
     for i in c.ids:
-        id_str += i +' '
+        id_str += i + " "
     f_w.write(id_str)
     f_w.close()
-
