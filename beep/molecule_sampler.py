3fefae51365531b4a93fdd13d3527a615c2cd59d
stvogt <stvogtgeisse@gmail.com>
2022-12-13 11:58:09 -0300

    black

--- beep/molecule_sampler.py
+++ beep/molecule_sampler.py
@@ -10,7 +10,7 @@ def random_molecule_sampler(
     target_molecule,
     number_of_structures=10,
     sampling_shell=2.5,
-    #save_xyz=[],
+    # save_xyz=[],
     print_out=False,
 ):
     out_string = """
@@ -33,7 +33,6 @@ def random_molecule_sampler(
     angst2bohr = constants.conversion_factor("angstrom", "bohr")
 
     fill_num = len(str(number_of_structures))
-
 
     # Define the maximum  and minimum displacements
     dis_min = max([np.linalg.norm(i) for i in cluster.geometry])  # remove 0.75
@@ -118,274 +117,266 @@ def random_molecule_sampler(
         fix_orientation=False,
         validated=False,
     )
-    #if save_xyz:
+    # if save_xyz:
     #    molecules_shifted.to_file(save_xyz + "/all.xyz")
-    #out_string += """ Thank you for using Molecule sampling! """
-    #if print_out:
+    # out_string += """ Thank you for using Molecule sampling! """
+    # if print_out:
     #    print(out_string)
     return molecules
 
+
 def single_site_spherical_sampling(
-    cluster_id,              #cluster + target_molecule
-    sampled_id,              #sampled_molecule
-    target_molecule,         #number of atoms
-    sampling_shell= 2.5,
-    grid_size = "normal",
+    cluster_id,  # cluster + target_molecule
+    sampled_id,  # sampled_molecule
+    target_molecule,  # number of atoms
+    sampling_shell=2.5,
+    grid_size="normal",
     save_xyz=[],
-    purge = 0.5,  # None if no purging is desired
-    theta_rot = None,
-    noise = True,
-    zenith_angle = np.pi/2
-    print_out=False):
-    
+    purge=0.5,  # None if no purging is desired
+    theta_rot=None,
+    noise=True,
+    zenith_angle=np.pi / 2,
+    print_out=False,
+):
+
     bohr2angst = constants.conversion_factor("bohr", "angstrom")
     angst2bohr = constants.conversion_factor("angstrom", "bohr")
-    
+
     def _Ry(rad):
-        '''Rotation matrix around y axis'''
-        return np.array([[np.cos(rad), 0.0, np.sin(rad)],
-                         [0.0, 1.0, 0.0],
-                         [-np.sin(rad), 0.0, np.cos(rad)]])
-    
+        """Rotation matrix around y axis"""
+        return np.array(
+            [
+                [np.cos(rad), 0.0, np.sin(rad)],
+                [0.0, 1.0, 0.0],
+                [-np.sin(rad), 0.0, np.cos(rad)],
+            ]
+        )
+
     def _Rx(rad):
-        '''Rotation matrix around x axis'''
-        return np.array([[1.0, 0.0, 0.0], 
-                         [0.0, np.cos(rad), -np.sin(rad)],
-                         [0.0, np.sin(rad), np.cos(rad)]])
+        """Rotation matrix around x axis"""
+        return np.array(
+            [
+                [1.0, 0.0, 0.0],
+                [0.0, np.cos(rad), -np.sin(rad)],
+                [0.0, np.sin(rad), np.cos(rad)],
+            ]
+        )
 
     def _Rz(rad):
-        '''Rotation matrix around z axis'''
-        return np.array([[np.cos(rad), -np.sin(rad), 0.0],
-                        [np.sin(rad), np.cos(rad), 0.0], 
-                        [0.0, 0.0, 1.0]])
-    
-    def _com(x,y):
-        '''Center of mass. x is geom and y the symbols'''
+        """Rotation matrix around z axis"""
+        return np.array(
+            [
+                [np.cos(rad), -np.sin(rad), 0.0],
+                [np.sin(rad), np.cos(rad), 0.0],
+                [0.0, 0.0, 1.0],
+            ]
+        )
+
+    def _com(x, y):
+        """Center of mass. x is geom and y the symbols"""
         num = 0
         den = 0
-        
+
         cont = 0
         for i in x:
-            num = num + (qcel.periodictable.to_mass(y[cont])*i)
+            num = num + (qcel.periodictable.to_mass(y[cont]) * i)
             den = den + qcel.periodictable.to_mass(y[cont])
             cont += 1
-            
-        com = num/den
-        return com
-    
 
-    
+        com = num / den
+        return com
+
     if grid_size == "normal":
-        grid = (4,12)
+        grid = (4, 12)
     if grid_size == "sparse":
-        grid = (4,8)
+        grid = (4, 8)
     if grid_size == "tight":
-        grid = (5,16)
-     
-    #Generate the grid
+        grid = (5, 16)
+
+    # Generate the grid
     radio = sampling_shell * angst2bohr
-    
-    phi_end = zenith_angle 
-    phi_interval = phi_end/(2*grid[0])
+
+    phi_end = zenith_angle
+    phi_interval = phi_end / (2 * grid[0])
     phi_noise = []
 
     theta_end = 2 * np.pi
-    theta_interval = theta_end/(1.5*grid[1])
+    theta_interval = theta_end / (1.5 * grid[1])
     theta_noise = []
-        
-    theta = np.linspace(0, theta_end, grid[0] )
+
+    theta = np.linspace(0, theta_end, grid[0])
     phi = np.linspace(0, phi_end, grid[1])
 
     for i in range(grid[0]):
         phi_noise.append(random.uniform(-phi_interval, phi_interval))
     for i in range(grid[1]):
         theta_noise.append(random.uniform(-theta_interval, theta_interval))
-    
 
-    phi = np.linspace(0, phi_end, grid[0], endpoint=False) #+ np.array(phi_noise)
-    theta = np.linspace(0, theta_end, grid[1], endpoint=False) #+ np.array(theta_noise)
+    phi = np.linspace(0, phi_end, grid[0], endpoint=False)  # + np.array(phi_noise)
+    theta = np.linspace(
+        0, theta_end, grid[1], endpoint=False
+    )  # + np.array(theta_noise)
 
-    #Grid
-    grid_xyz_i = [[0,0,radio]] 
-        
+    # Grid
+    grid_xyz_i = [[0, 0, radio]]
+
     for i in theta:
         for n in phi[1:]:
             grid_p = []
-            
+
             r = radio
-            r += r * random.random()/7.
-                
-            x = r*np.sin(n)*np.cos(i)
-            y = r*np.sin(n)*np.sin(i)
-            z = r*np.cos(n)
-                
-            grid_p.append(x)# * bohr2angst)
-            grid_p.append(y)# * bohr2angst)
-            grid_p.append(z)# * bohr2angst)
-            
-            
-            
+            r += r * random.random() / 7.0
+
+            x = r * np.sin(n) * np.cos(i)
+            y = r * np.sin(n) * np.sin(i)
+            z = r * np.cos(n)
+
+            grid_p.append(x)  # * bohr2angst)
+            grid_p.append(y)  # * bohr2angst)
+            grid_p.append(z)  # * bohr2angst)
+
             grid_xyz_i.append(grid_p)
-                
-                
-    #purge
-        
+
+    # purge
+
     grid_xyz = grid_xyz_i.copy()
     if purge:
         for i in range(0, len(grid_xyz)):
             for j in range(i + 1, len(grid_xyz)):
-                if np.linalg.norm(np.array(grid_xyz_i[i]) - np.array(grid_xyz_i[j])) <= purge: 
+                if (
+                    np.linalg.norm(np.array(grid_xyz_i[i]) - np.array(grid_xyz_i[j]))
+                    <= purge
+                ):
                     grid_xyz.remove(grid_xyz_i[j])
                     print("removing point")
 
-    
-
-        
     print("Total grid points: ", len(grid_xyz))
-                                                              
+
     cluster = client.query_molecules(cluster_id)[0]
     sampled = client.query_molecules(sampled_id)[0]
-                                                              
-    #target
-    
-    #Define unit vectors
+
+    # target
+
+    # Define unit vectors
     vz = np.array([0.0, 0.0, 1.0])
     vy = np.array([0.0, 1.0, 0.0])
     vx = np.array([1.0, 0.0, 0.0])
 
-
-    # Get center of mass of target molecule 
+    # Get center of mass of target molecule
     len_target = int(target_molecule)
-    target_mol_geom = cluster.geometry[-len_target:,:]
+    target_mol_geom = cluster.geometry[-len_target:, :]
     target_mol_symb = cluster.symbols[-len_target:]
     com_target = _com(target_mol_geom, target_mol_symb)
 
     print("Original center of mass of  cluster:  ", -com_target)
-    #Shift to set the origin at the center of mass of target molecule 
-    cluster_tras = cluster.scramble(do_shift=-com_target, do_rotate=False, do_resort=False)[0]
-       
-    #Get geometries of shifted cluster    
+    # Shift to set the origin at the center of mass of target molecule
+    cluster_tras = cluster.scramble(
+        do_shift=-com_target, do_rotate=False, do_resort=False
+    )[0]
+
+    # Get geometries of shifted cluster
     cluster_geom = cluster_tras.geometry
     cluster_symb = cluster_tras.symbols
-    
-    
+
     cluster_geom_s = []
     for i in cluster_geom:
-        new_coords =  np.multiply(i, np.sign(-com_target))
+        new_coords = np.multiply(i, np.sign(-com_target))
         cluster_geom_s.append(new_coords)
-        
+
     com_refl = _com(cluster_geom_s, cluster_symb)
-        
-    print("center of mass of adjusted cluster: ",com_refl)
-    
-    # Define rotation angles 
-    theta_rot_rz = np.arccos(np.dot(com_refl[:-1], vy[:-1]) / np.linalg.norm(com_refl[:-1]))
-    print("Rotation angle around the z axis (xy plane)", theta_rot_rz * 180./np.pi)
-                                                 
+
+    print("center of mass of adjusted cluster: ", com_refl)
+
+    # Define rotation angles
+    theta_rot_rz = np.arccos(
+        np.dot(com_refl[:-1], vy[:-1]) / np.linalg.norm(com_refl[:-1])
+    )
+    print("Rotation angle around the z axis (xy plane)", theta_rot_rz * 180.0 / np.pi)
+
     cluster_rot_geom_rz = []
     for i in cluster_geom_s:
         cluster_rot_geom_rz.append(np.dot(_Rz(theta_rot_rz), i))
-        
+
     v_check_1 = np.dot(_Rz(theta_rot_rz), -com_target)
-        
+
     print("Rotation check: ", v_check_1)
 
-        
     com_int = _com(cluster_rot_geom_rz, cluster_symb)
     print("Intermediate center of mass:", com_int)
-    
+
     # Define rotation angle around x axis
     theta_rot_rx = np.arccos(np.dot(com_int[1:], vz[1:]) / np.linalg.norm(com_int[1:]))
-    print("Rotation angle around the x axis (yz plane)", theta_rot_rx * 180./np.pi)
+    print("Rotation angle around the x axis (yz plane)", theta_rot_rx * 180.0 / np.pi)
 
-    
     cluster_rot_geom = []
     for i in cluster_rot_geom_rz:
-        new_coords = np.dot(_Rx(theta_rot_rx), i) 
-        cluster_rot_geom.append(np.multiply(new_coords, np.array([1.,1.,-1.])))
-    
+        new_coords = np.dot(_Rx(theta_rot_rx), i)
+        cluster_rot_geom.append(np.multiply(new_coords, np.array([1.0, 1.0, -1.0])))
+
     v_check_2 = np.dot(_Rx(theta_rot_rx), v_check_1)
-    
+
     print("Rotation check: ", v_check_2)
 
-    
     com_final = _com(cluster_rot_geom, cluster_symb)
     print("Final center of mass:", com_final)
 
-    
-    theta_check = np.arccos(np.dot(com_final , vz) / np.linalg.norm(com_final))
-    print("Final angle: ", theta_check * 180./np.pi)
+    theta_check = np.arccos(np.dot(com_final, vz) / np.linalg.norm(com_final))
+    print("Final angle: ", theta_check * 180.0 / np.pi)
 
- 
-    #return None
-    
-    #prepare sampled molecule
+    # return None
+
+    # prepare sampled molecule
     sampled_molecule_len = len(sampled.geometry)
     sampled_com = _com(sampled.geometry, sampled.symbols)
-    #print(sampled_com)
-    sampled_mol = sampled.scramble(do_shift = -1*sampled_com, do_rotate=False, do_resort=False)[0]
-    
-    #generated estructures 
+    # print(sampled_com)
+    sampled_mol = sampled.scramble(
+        do_shift=-1 * sampled_com, do_rotate=False, do_resort=False
+    )[0]
+
+    # generated estructures
     molecules = []
-                   
+
     vis_mol_geom = list(np.array(cluster_rot_geom).flatten())
     vis_mol_atms = list(cluster_symb)
 
-    #generate the new structures
+    # generate the new structures
     for i in grid_xyz:
-                                                              
-        #move the center of mass of sampled molecule to the point i in grid 
-        shift_vector = np.array(i)*angst2bohr
-        sampled_final_mol = sampled_mol.scramble(do_shift=shift_vector, do_rotate=True, do_resort=False)[0]
-                                                              
+
+        # move the center of mass of sampled molecule to the point i in grid
+        shift_vector = np.array(i) * angst2bohr
+        sampled_final_mol = sampled_mol.scramble(
+            do_shift=shift_vector, do_rotate=True, do_resort=False
+        )[0]
+
         sampled_geom = sampled_final_mol.geometry
         sampled_symb = sampled_final_mol.symbols
-                                                              
-        #create the new structure 
+
+        # create the new structure
         atms = list(cluster_symb)
         sampled_atms = list(sampled_symb)
         atms.extend(sampled_atms)
         vis_mol_atms.extend("H")
 
-                                                              
         geom = list(np.array(cluster_rot_geom).flatten())
         sampled_geom = list(np.array(sampled_geom).flatten())
         geom.extend(sampled_geom)
         vis_mol_geom.extend(list(i))
 
-                                                              
-        molecule = qcel.models.Molecule(symbols=atms,
-                                        geometry=geom,
-                                        fix_com=True,
-                                        fix_orientation=True)
+        molecule = qcel.models.Molecule(
+            symbols=atms, geometry=geom, fix_com=True, fix_orientation=True
+        )
         molecules.append(molecule)
-    
-    #vis_mol_atms.extend("B")
-    #vis_mol_geom.extend([0.0, 0.0, -30.0])
-    #vis_mol_atms.extend("B")
-    #vis_mol_geom.extend([0.0, 0.0, 22.0])
 
+    # vis_mol_atms.extend("B")
+    # vis_mol_geom.extend([0.0, 0.0, -30.0])
+    # vis_mol_atms.extend("B")
+    # vis_mol_geom.extend([0.0, 0.0, 22.0])
 
-    all_mols = qcel.models.Molecule(symbols=vis_mol_atms,
-                                    geometry=vis_mol_geom,
-                                    fix_com=True,
-                                    fix_orientation=True)
-                                                              
+    all_mols = qcel.models.Molecule(
+        symbols=vis_mol_atms, geometry=vis_mol_geom, fix_com=True, fix_orientation=True
+    )
+
     return molecules, all_mols
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -438,9 +429,8 @@ def main():
         options.s_mol,
         number_of_structures=options.s_num,
         sampling_shell=options.sampling_shell,
-        #save_xyz=options.xyz_path,
+        # save_xyz=options.xyz_path,
         print_out=options.print_out,
     )
 
     return
-
