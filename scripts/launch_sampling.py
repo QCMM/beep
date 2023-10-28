0fab118f79b96611d851c49a113e9d8a4b46df3d
stvogt <stvogtgeisse@gmail.com>
2023-10-28 18:07:43 -0300

    new improved launch_sampling

--- scripts/launch_sampling.py
+++ scripts/launch_sampling.py
@@ -1,15 +1,51 @@
-import sys, time
-import qcfractal.interface as ptl
+import sys, time, argparse, logging
 from pathlib import Path
+import qcfractal.interface as ptl
 from beep.converge_sampling import sampling
-import argparse
+from beep.errors import DatasetNotFound, LevelOfTheoryNotFound
+
+welcome_msg = """       
+                  Welcome to the BEEP binding sites sampler! 
+
+Description: The BEEP binding sites sampler optimizes random initial configurations
+of a small molecule around a set-of-clusters surface model,  until a default of 250 binding 
+sites are found..
+
+Author: svogt, gbovolenta
+
+            """
+
+
+def sampling_model_msg(surface_model, target_mol, method, basis, program):
+    return """
+------------------------------------------------------------------------------
+Starting the sampling of the surface model {0} with the molecule {1}
+The sampling level of theory is method : {2} basis: {3} program {4}
+------------------------------------------------------------------------------
+
+    """.format(
+        surface_model, target_mol, method, basis, program
+    )
+
+
+def sampling_round_msg(name_cluster, opt_smplg, opt_refine):
+    return """
+##############################################################################
+Processing cluster: {}
+The OptimizationDataset for the sampling results: {}
+The OptimiztionDataset for the unique structures for refinement: {}
+
+    """.format(
+        name_cluster, opt_smplg, opt_refine
+    )
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
         description="""
-    A command line interface to sample the surface of a set of water clusters (stored in a 
-    QCFractal DataSet) with a small molecule or atom. This CLI is part
-    of the Binding Energy Evaluation Platform (BEEP).
+A command line interface to sample the surface of a set of water clusters (stored in a 
+QCFractal DataSet) with a small molecule or atom. This CLI is part
+of the Binding Energy Evaluation Platform (BEEP).
     """
     )
 
@@ -34,211 +70,276 @@ def parse_arguments():
         help="The name of the molecule to be sampled (from a QCFractal OptimizationDataSet collection)",
     )
     parser.add_argument(
-        "--surface_model_collection",
+        "--surface-model-collection",
         default="Water_22",
         help="The name of the collection with the set of water clusters (default: Water_22)",
     )
     parser.add_argument(
-        "--small_molecule_collection",
+        "--small-molecule-collection",
         default="Small_molecules",
         help="The name of the collection containing molecules or radicals (default: Small_molecules)",
     )
     parser.add_argument(
-        "--sampling_shell",
+        "--sampling-shell",
         type=float,
         default=2.0,
-        help="The shell size of sampling space in Angstrom (Default = 2.0)",
+        help="The shell size of the sampling space in Angstrom (Default = 2.0)",
     )
     parser.add_argument(
-        "--sampling_condition",
+        "--total-binding-sites",
+        type=int,
+        default=220,
+        help="The total binding sites for the distribution (Default = 250)",
+    )
+    parser.add_argument(
+        "--sampling-condition",
         type=str,
         default="normal",
         help="How tight the sampling should be done for each surface. Options: sparse, normal, fine (Default: normal)",
     )
     parser.add_argument(
-        "--level_of_theory",
-        default="blyp_def2-svp",
-        help="The level of theory in the format: method_basis (default: blyp_def2-svp)",
+        "--sampling-level-of-theory",
+        nargs=3,
+        default=["blyp", "def2-svp", "terachem"],
+        help="The level of theory in the format: method basis program (default: blyp def2-svp terachem)",
     )
     parser.add_argument(
-        "--refinement_level_of_theory",
-        default="hf3c_minix",
-        help="The level of theory for geometry refinement in the format: method_basis (default: hf3c_minix)",
+        "--refinement-level-of-theory",
+        nargs=3,
+        default=["bhandhlyp", "def2-svp", "psi4"],
+        help="The level of theory and program for the geometry refinement in the format: method_basis (default: bhandhlyp def2-svp psi4)",
     )
     parser.add_argument(
-        "--rmsd_value",
+        "--rmsd-value",
         type=float,
         default=0.40,
-        help="Rmsd geometrical criteria, all structures below this value will not be considered as unique. (default: 0.40 angstrom)",
+        help="RMSD geometrical criteria, all structures below this value will not be considered as unique. (default: 0.40 angstrom)",
     )
     parser.add_argument(
-        "--rmsd_symmetry",
+        "--rmsd-symmetry",
         action="store_true",
-        help="Consider the molecular symmetry for the rmsd calculation",
+        help="Consider the molecular symmetry for the RMSD calculation",
     )
     parser.add_argument(
-        "--program",
-        default="psi4",
-        help="The program to use for this calculation (default: psi4)",
-    )
-    parser.add_argument(
-        "--sampling_tag",
+        "--sampling-tag",
         default="sampling",
         help="The tag to use to specify the qcfractal-manager for the sampling optimization (default: sampling)",
     )
     parser.add_argument(
-        "--keyword_id",
+        "--keyword-id",
         default=None,
-        help="ID of the QC keywords for the OptimizationDataSet specification (default: None)",
+        help="ID of the QC keywords for the OptimizationDataSet specification of the sampling (default: None)",
     )
 
     return parser.parse_args()
 
 
-options = parse_arguments()
-
-username = options.usern
-password = options.passwd
-method = options.level_of_theory.split("_")[0]
-basis = options.level_of_theory.split("_")[1]
-program = options.program
-tag = options.tag
-opt_lot = options.level_of_theory
-r_lot = options.r_level_of_theory
-kw_id = options.keyword_id
-sampling_shell = options.sampling_shell
-sampling_condition = options.sampling_condition
-rmsd_symm = options.rmsd_symmetry
-cluster_collection = options.surface_model_collection
-mol_collection = options.small_molecule_collection
-smol_name = options.molecule
-rmsd_val = options.rmsd_val
-
-client = ptl.FractalClient(
-    address=options.client_address, verify=False, username=username, password=password
-)
-
-m = r_lot.split("_")[0]
-b = r_lot.split("_")[1]
+def sampling_args(args):
+    return {
+        "method": args.sampling_level_of_theory[0],
+        "basis": args.sampling_level_of_theory[1],
+        "program": args.sampling_level_of_theory[2],
+        "tag": args.sampling_tag,
+        "kw_id": args.keyword_id,
+        "opt_lot": args.sampling_level_of_theory[0]
+        + "_"
+        + args.sampling_level_of_theory[1],
+        "rmsd_symm": args.rmsd_symmetry,
+        "rmsd_val": args.rmsd_value,
+        "sampling_shell": args.sampling_shell,
+        "sampling_condition": args.sampling_condition,
+    }
 
 
-spec = {
-    "name": m + "_" + b,
-    "description": "Geometric + Psi4/" + m + "/" + b,
-    "optimization_spec": {"program": "geometric", "keywords": None},
-    "qc_spec": {
-        "driver": "gradient",
-        "method": m,
-        "basis": b,
-        "keywords": None,
-        "program": "psi4",
-    },
-}
-
-count = 0
-
-try:
-    ds_soc = client.get_collection("OptimizationDataset", cluster_collection)
-except KeyError:
-    print(
-        """Collection with set of clusters that span the surface {} does not exist. Please create it first. Exiting...
-    """.format(
-            cluster_collection
-        )
-    )
-    sys.exit(1)
-
-try:
-    ds_sm = client.get_collection("OptimizationDataset", mol_collection)
-except KeyError:
-    print(
-        """Collection {} with the target molecules does not exist, please create it first. Exiting...
-    """.format(
-            mol_collection
-        )
-    )
-    sys.exit(1)
-
-soc_list = ds_soc.data.records
-
-try:
-    target_mol = ds_sm.get_record(smol_name, opt_lot).get_final_molecule()
-except KeyError:
-    print(
-        "{} is not optimized at the requested level of theory, please optimize them first\n".format(
-            smol_name
-        )
-    )
-    sys.exit(1)
-
-for w in ds_soc.data.records:
-    print("Processing cluster: {}".format(w))
-    try:
-        cluster = ds_soc.get_record(w, opt_lot).get_final_molecule()
-    except KeyError:
-        print(
-            "{} is not optimized at the requested level of theory, please optimize it first\n".format(
-                w
-            )
-        )
-        continue
-
-    opt_dset_name = smol_name + "_" + w
-
-    try:
-        ds_opt = client.get_collection("OptimizationDataset", opt_dset_name)
-        c = len(ds_opt.status(collapse=False))
-        count = count + int(c)
-    except KeyError:
-        pass
-
-    out_file = Path("./site_finder/" + str(smol_name) + "_w/" + w + "/out_sampl.dat")
-
-    if not out_file.is_file():
-        out_file.parent.mkdir(parents=True, exist_ok=True)
-
-    with open(out_file, "w") as f:
-        f.write(
-            """        Welcome to the Molecular Sampler Converger! 
-    
-    Description: The molecular sampler converger optimizes random initial configurations
-    of a small molecule around a  center molecule until at least 220 binding sites are found or
-    all clusters of a set are sampled..
-    
-    Author: svogt, gbovolenta
-    Data: 10/24/2020
-    
+def check_collection_existence(
+    client, *collections, collection_type="OptimizationDataset"
+):
     """
-        )
-    sampling(
-        method,
-        basis,
-        program,
-        tag,
-        kw_id,
-        opt_dset_name,
-        opt_lot,
-        rmsd_symm,
-        rmsd_val,
-        target_mol,
-        cluster,
-        out_file,
-        client,
-        sampling_shell,
-        sampling_condition,
-    )
-    print("Total number of binding sites so far: {} ".format(count))
-    ds_opt = client.get_collection("OptimizationDataset", opt_dset_name)
+    Check the existence of collections and raise DatasetNotFound error if not found.
+
+    Args:
+    - client: The client used for collection retrieval.
+    - *collections: Variable number of collection names to check for existence.
+
+    Raises:
+    - DatasetNotFound: If any of the specified collections do not exist.
+    """
+    for collection in collections:
+        try:
+            client.get_collection(collection_type, collection)
+        except KeyError:
+            raise DatasetNotFound(
+                f"Collection {collection} does not exist. Please create it first. Exiting..."
+            )
+
+
+def check_optimized_molecule(ds, opt_lot, mol_names):
+    for mol in list(mol_names):
+        try:
+            rr = ds.get_record(mol, opt_lot)
+        except KeyError:
+            raise LevelOfTheoryNotFound(
+                f"{opt_lot} level of theory for {mol} or the entry itself does not exist. Add the molecule and optimize it first\n"
+            )
+        if rr.status == "INCOMPLETE":
+            raise ValueError(f" Optimization has status {rr.status} restart it or wait")
+        elif rr.status == "ERROR":
+            raise ValueError(f" Optimization has status {rr.status} restart it or wait")
+
+
+# def get_dataset_entry_num(ds):
+#    #df = ds.status(opt_lot, collapse=False)
+#    #num = len(df[df[opt_lot] == "COMPLETE"])
+#    return len(
+
+
+def get_or_create_opt_collection(client, dset_name):
+    try:
+        ds_opt = client.get_collection("OptimizationDataset", dset_name)
+        out_string = f"OptimizationDataset {dset_name} already exists, new sampled structures will be saved here."
+    except KeyError:
+        ds_opt = ptl.collections.OptimizationDataset(dset_name, client=client)
+        ds_opt.save()
+        ds_opt = client.get_collection("OptimizationDataset", dset_name)
+        out_string = f"Creating new OptimizationDataset: {dset_name}."
+    return ds_opt
+
+
+def process_refinement(client, refine_lot, ds_opt, logger):
+    method, basis, program = refine_lot
+    spec = {
+        "name": method + "_" + basis,
+        "description": f"Geometric + {method}/{basis}/{program}",
+        "optimization_spec": {"program": "geometric", "keywords": None},
+        "qc_spec": {
+            "driver": "gradient",
+            "method": method,
+            "basis": basis,
+            "keywords": None,
+            "program": program,
+        },
+    }
     ds_opt.add_specification(**spec, overwrite=True)
     ds_opt.save()
-    c = ds_opt.compute(m + "_" + b, tag="refinement")
-    count = count + int(c)
-    with open(out_file, "a") as f:
-        f.write(
-            """
-        Sending {} optimizations at {} level.""".format(
-                c, r_lot
-            )
+    num_of_bs = len(ds_opt.data.records)
+    logger.info(f"{num_of_bs} unique binding sites found in this round.")
+    c = ds_opt.compute(method + "_" + basis, tag="refinement")
+    logger.info(f"Sending {c} optimizations at {refine_lot} level.")
+
+
+def main():
+    # Call the arguments
+    args = parse_arguments()
+
+    # Create a logger
+    logger = logging.getLogger("beep_logger")
+    # logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
+
+    # File handler for logging to a file
+    log_file = (
+        "beep_sampling_" + args.molecule + "_" + args.surface_model_collection + ".log"
+    )
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setFormatter(logging.Formatter("%(message)s"))
+    logger.addHandler(file_handler)
+
+    # Console handler for logging to stdout
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(logging.Formatter("%(message)s"))
+    logger.addHandler(console_handler)
+
+    logger.info(welcome_msg)
+
+    client = ptl.FractalClient(
+        address=args.client_address,
+        verify=False,
+        username=args.username,
+        password=args.password,
+    )
+
+    # The name of the molecule to be sampled at level of theory opt_lot
+    smol_name = args.molecule
+    method = args.sampling_level_of_theory[0]
+    basis = args.sampling_level_of_theory[1]
+    program = args.sampling_level_of_theory[2]
+    opt_lot = method + "_" + basis
+
+    # Check if the OptimizationDataSets exist
+    check_collection_existence(
+        client, args.surface_model_collection, args.small_molecule_collection
+    )
+    ds_sm = client.get_collection("OptimizationDataset", args.small_molecule_collection)
+    ds_wc = client.get_collection("OptimizationDataset", args.surface_model_collection)
+
+    # Check if all the molecules are optimized at the requested level of theory
+    check_optimized_molecule(ds_sm, opt_lot, [smol_name])
+    check_optimized_molecule(ds_wc, opt_lot, ds_wc.data.records.keys())
+
+    args_dict = sampling_args(args)
+    args_dict["target_mol"] = ds_sm.get_record(smol_name, opt_lot).get_final_molecule()
+    args_dict["client"] = client
+
+    count = 0
+
+    logger.info(
+        sampling_model_msg(
+            args.surface_model_collection, smol_name, method, basis, program
         )
-    if count > 220:
-        break
+    )
+
+    for c, w in enumerate(ds_wc.data.records):
+        # Getting cluster record
+        args_dict["cluster"] = ds_wc.get_record(w, opt_lot).get_final_molecule()
+
+        # Defining collection names and passing the to argunment dict
+        smol_name = args.molecule
+        ref_opt_dset_name = smol_name + "_" + w
+        smplg_opt_dset_name = "pre_" + ref_opt_dset_name
+        args_dict["opt_dset_name"] = ref_opt_dset_name
+
+        # Retrieve or create the Sampling collection
+        ds_smplg = get_or_create_opt_collection(client, smplg_opt_dset_name)
+        ds_ref = get_or_create_opt_collection(client, ref_opt_dset_name)
+
+        ## logging info for sampling cluster
+        logger.info(sampling_round_msg(w, smplg_opt_dset_name, ref_opt_dset_name))
+
+        try:
+            old_opt_num = len(ds_ref.data.records)
+            logger.debug(
+                f"Number of existing binding sites for this cluster: {old_opt_num}"
+            )
+            count = count + old_opt_num
+        except KeyError:
+            pass
+
+        # Logging
+        out_file = Path(
+            "./site_finder/" + str(smol_name) + "_w/" + w + "/out_sampl.dat"
+        )
+        if not out_file.is_file():
+            out_file.parent.mkdir(parents=True, exist_ok=True)
+        with open(out_file, "w") as f:
+            f.write(welcome_msg)
+        args_dict["o_file"] = out_file
+
+        sampling(**args_dict)
+
+        logger.info(f"Total number of binding sites so far: {count}")
+        r_lot = args.refinement_level_of_theory
+        process_refinement(client, r_lot, ds_ref, logger)
+        count = count + int(c)
+        if count > args.total_binding_sites:
+            logger.info(
+                f"Thank you for using BEEP binding site sampler. Total binding sites : {count}\n"
+            )
+            break
+
+    logger.info(
+        f"Thank you for using BEEP binding site sampler. Total binding sites : {count}\n"
+    )
+
+
+if __name__ == "__main__":
+    main()
